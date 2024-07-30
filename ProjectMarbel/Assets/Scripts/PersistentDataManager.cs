a25420407a84c55fecf382a7ab498d238b2d33e2
peterligeti <134300959+peterligeti@users.noreply.github.com>
2024-07-31 00:29:27 +0800

    Persistent data manager v1 added

---
+++ ProjectMarbel/Assets/Scripts/PersistentDataManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// How to use it to store data when switching between scene changes:
+// example: when changing the master volume level that needs to be stored:
+// SET:
+//  if (MainManager.Instance != null)
+// {
+//     PersistentDataManager.Instance.masterVolumeLevel = 0.1f;
+// }
+// GET
+// if (PersistentDataManager.Instance != null)
+// {
+//     float volume = PersistentDataManager.Instance.masterVolumeLevel;
+// }
+
+public class PersistentDataManager : MonoBehaviour
+{
+    public static PersistentDataManager Instance;
+
+    // marbles selected by players
+    public GameObject player1CurrentMarble;
+    public GameObject player2CurrentMarble;
+    
+    // settings menu items, these are saved between sessions as well
+    public float masterVolumeLevel;
+    public float vfxVolumeLevel;
+    public float musicVolumeLevel;
+
+    public int graphicsResolution;
+    public bool graphicsFullscreen;
+    
+    private void Awake()
+    {
+        if (Instance != null) // singleton pattern, only one gameObject with this script can exist
+        {
+            // Debug.Log("PersistentDataManager instance destroyed");
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject); // gameObject with this script will not be destroyed when switching scenes
+    }
+}
