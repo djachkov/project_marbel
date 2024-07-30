1a123d3adc7bb765647370defc1693ae928d06bc
Moh. Yosry Zorainy <mohamedu3@gmail.com>
2024-07-30 22:51:54 +0300

    Buildings are Destroyed only on Collision
    Edited the prototype scene. Buildings are now destroyed only if being hit by the ball (marble).
    Also, buildings now are fractured into pieces on hit, similar to real buildings, instead of feeling like cardboard buildings in the previous versions.

---
+++ ProjectMarbel/Assets/Scripts/Breakable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    [SerializeField] private GameObject _replacement;
+    [SerializeField] private float _breakForce = 2;
+    [SerializeField] private float _collisionMultiplier = 50;
+    [SerializeField] private bool _broken;
+
+    //[SerializeField] private AudioClip _explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (_broken) return;
+        if (collision.relativeVelocity.magnitude >= _breakForce)
+        {
+            _broken = true;
+            var replacement = Instantiate(_replacement, transform.position, transform.rotation);
+            
+            var rbs = replacement.GetComponentsInChildren<Rigidbody>();
+            foreach (var rb in rbs)
+            {
+                rb.AddExplosionForce(collision.relativeVelocity.magnitude * _collisionMultiplier, collision.contacts[0].point, 2);
+            }
+
+            //AudioSource.PlayClipAtPoint(_explosion,transform.position);
+            Destroy(gameObject);
+        }
+    }
+}
