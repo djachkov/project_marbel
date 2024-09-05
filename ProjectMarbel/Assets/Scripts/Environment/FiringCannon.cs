33e028d1fdd41872f9ccefb4cd2742422b8f8220
Moh. Yosry Zorainy <mohamedu3@gmail.com>
2024-09-05 17:08:40 +0300

    Enhancing the Cannon Firing
    - The FiringCannon script was refactored.
    - The Cannonball Gameobjects are destroyed after being fired and doing the damage to the castle walls. This was done by the script "DestroyObject" which can be modified to destroy other objects too if needed.

--- ProjectMarbel/Assets/Scripts/Environment/FiringCannon.cs
+++ ProjectMarbel/Assets/Scripts/Environment/FiringCannon.cs
@@ -4,36 +4,36 @@ using UnityEngine;
 
 public class FiringCannon : MonoBehaviour
 {
-    public GameObject cannonBall;    // the cannon ball prefab
-    private Vector3 spawnPos;    // cannon ball's instantiation position
-    private Vector3 spawnRot;    // cannon ball's instantiation rotation
-    private GameObject newCannonBall;
-
-    Rigidbody newCannonBallRB;     // cannon ball's rigidbody 
-    public float force = 60.0f;   // store the force required to fire the cannon ball
-
+    public GameObject cannonBall;    // the cannonball prefab
+    private float lastFiringTime;    // stores the last time a cannonball was fired
+    [SerializeField] float force = 60.0f;   // store the force required to fire the cannon ball
+    [SerializeField] float timeInterval = 20.0f;   // A cannon ball is fired each timeInterval
 
     // Start is called before the first frame update
     void Start()
     {
-
+        lastFiringTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Time.time - lastFiringTime >= timeInterval)
         {
-            spawnPos = transform.position;
-            //spawnRot = transform.rotation.eulerAngles;
-            //Debug.Log(spawnRot);
-            //spawnRot = new Vector3(spawnRot.x, spawnRot.y, spawnRot.z + 180);
-            //Debug.Log(spawnRot);
-            newCannonBall = Instantiate(cannonBall, spawnPos, Quaternion.identity);
-            newCannonBall.tag = "CannonBall";
-            newCannonBallRB = newCannonBall.GetComponent<Rigidbody>();
-            newCannonBallRB.AddForce(-transform.forward * force, ForceMode.Impulse);
+            FireCannonBall();
+            lastFiringTime = Time.time;
         }
     }
 
+    void FireCannonBall()
+    {
+        GameObject newCannonBall;   //stores the new instance of the cannonball prefab
+        Rigidbody newCannonBallRB;     // store the cannonball instance's rigidbody 
+        Vector3 spawnPos;    // cannon ball's instantiation position
+        spawnPos = transform.position;
+        newCannonBall = Instantiate(cannonBall, spawnPos, Quaternion.identity);
+        newCannonBall.tag = "CannonBall";
+        newCannonBallRB = newCannonBall.GetComponent<Rigidbody>();
+        newCannonBallRB.AddForce(-transform.forward * force, ForceMode.Impulse);
+    }
 }
