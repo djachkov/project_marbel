127280bbc81413058495e6525befbabb5b0446f8
Dmitrii Diachkov <djachkov.dmitrij@gmail.com>
2024-09-08 23:24:46 +0200

    Simple script for displaying game controls

---
+++ ProjectMarbel/Assets/Scripts/UI/ShowControls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowControls : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text controlsText;
+    [SerializeField]
+    private float timeLimit = 30f;
+
+    void Start()
+    {
+        StartCoroutine(HideControlsAfterDelay());
+    }
+
+    IEnumerator HideControlsAfterDelay()
+    {
+        controlsText.enabled = true;
+        yield return new WaitForSeconds(timeLimit);
+        controlsText.enabled = false;
+    }
+}
