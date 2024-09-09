e06e7eedeebfd56c25913b4a9746e63412ec44f6
Moh. Yosry Zorainy <mohamedu3@gmail.com>
2024-09-09 09:59:09 +0300

    Fixed Rubbles Cleaning after Building Destruction
    Fixed Rubbles Cleaning after Building Destruction using the script RubbleCleaning. To do its job, all rubbles parts where tagged "Building Parts".

---
+++ ProjectMarbel/Assets/Scripts/Environment/RubbleCleaning.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubbleCleaning : MonoBehaviour
+{
+    [SerializeField] private GameObject[] rubbleParts;
+    private float lastCleaningTime;    // stores the last time the rubbles were cleaned
+    [SerializeField] float timeInterval = 3.0f;   // The time interval between cleanings
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastCleaningTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - lastCleaningTime >= timeInterval)
+        {
+            CleanRubbles();
+            lastCleaningTime = Time.time;
+        }
+    }
+
+    void CleanRubbles()
+    {
+        rubbleParts = GameObject.FindGameObjectsWithTag("Building Parts");
+        
+        foreach (var part in rubbleParts)
+        {
+            var partRB = part.GetComponent<Rigidbody>();
+            if (partRB.velocity == Vector3.zero && partRB.angularVelocity == Vector3.zero)
+            {
+                Destroy(part);
+            }
+        }
+    }
+}
